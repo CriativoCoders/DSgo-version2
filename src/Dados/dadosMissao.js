254d970043e7e49e0a08edae640caa5f15654dc9
Talita Cristina Alves Lobato <05058225204@>
2025-11-24 11:09:04 -0300

    novo

--- src/Dados/dadosMissao.js
+++ src/Dados/dadosMissao.js
@@ -62,5 +62,86 @@ export const missoes = [
     descricao: "Um codigo de excelecia tem .... code",
     respostaCorreta: "Clean",
     status: 'disponivel'
-  }
+  },
+  {
+  id: 9,
+  titulo: "Tecnologia",
+  missao: "O que eu sou?",
+  descricao: "Um framework JavaScript criado pelo Facebook para construir interfaces de usuário...",
+  respostaCorreta: "React",
+  status: 'disponivel'
+},
+{
+  id: 10,
+  titulo: "Tecnologia",
+  missao: "Quem criou?",
+  descricao: "Linguagem de programação criada por Guido van Rossum em 1991...",
+  respostaCorreta: "Python",
+  status: 'disponivel'
+},
+{
+  id: 11,
+  titulo: "Tecnologia",
+  missao: "O que eu sou?",
+  descricao: "Linguagem usada para desenvolver apps Flutter...",
+  respostaCorreta: "Dart",
+  status: 'disponivel'
+},
+{
+  id: 12,
+  titulo: "Tecnologia",
+  missao: "O que eu sou?",
+  descricao: "Toolkit do Google para criar apps nativos para mobile, web e desktop com uma única base de código...",
+  respostaCorreta: "Flutter",
+  status: 'disponivel'
+},
+{
+  id: 13,
+  titulo: "Tecnologia",
+  missao: "Quem criou?",
+  descricao: "Sistema operacional criado por Linus Torvalds...",
+  respostaCorreta: "Linux",
+  status: 'disponivel'
+},
+{
+  id: 14,
+  titulo: "Tecnologia",
+  missao: "O que eu sou?",
+  descricao: "Banco de dados NoSQL orientado a documentos muito usado com Node.js...",
+  respostaCorreta: "MongoDB",
+  status: 'disponivel'
+},
+{
+  id: 15,
+  titulo: "Tecnologia",
+  missao: "Quem criou?",
+  descricao: "Empresa responsável pela criação do sistema Android...",
+  respostaCorreta: "Google",
+  status: 'disponivel'
+},
+{
+  id: 16,
+  titulo: "Tecnologia",
+  missao: "O que eu sou?",
+  descricao: "Plataforma e linguagem usada para desenvolvimento de páginas web no lado do cliente...",
+  respostaCorreta: "JavaScript",
+  status: 'disponivel'
+},
+{
+  id: 17,
+  titulo: "Tecnologia",
+  missao: "O que eu sou?",
+  descricao: "Gerenciador de pacotes utilizado em projetos JavaScript, especialmente com Node.js...",
+  respostaCorreta: "NPM",
+  status: 'disponivel'
+},
+{
+  id: 18,
+  titulo: "Tecnologia",
+  missao: "Quem criou?",
+  descricao: "Empresa que desenvolveu o framework .NET...",
+  respostaCorreta: "Microsoft",
+  status: 'disponivel'
+}
 ];
+
--- src/Dados/recompensas.js
+++ src/Dados/recompensas.js
@@ -1,12 +1,23 @@
 // Dados/recompensas.js
+import israel from '../assets/figurinha_israel.png'
 import dorival from '../assets/Dori.png';
 import marcia from '../assets/marciaMochila.png';
 import fernanda from '../assets/ferAvatar.png';
 import mariany from '../assets/maryAvatar.png';
-import israel from '../assets/trofeu.png';
 import marianyTecnica from '../assets/mary.png';
 import chavePrimaria from '../assets/trofeu.png';
 import cleanCode from '../assets/trofeu.png';
+import react from '../assets/figurinha_react.png';
+import python from '../assets/figurinha_python.png';
+import dart from '../assets/figurinha_dart.png';
+import flutter from '../assets/figrinha_flutter.png';
+import linux from '../assets/figurinha_linux.png';
+import mongoDB from '../assets/figurinha_mongo.png';
+import google from '../assets/figurinha_google.png';
+import javaScript  from '../assets/figurinha_js.png';
+import npm from '../assets/figurinha_NPM.png';
+import microsoft from '../assets/figurinha_microsoft.png';
+import React from 'react';
 
 export const recompensas = {
   1: { id: 1, nome: "Figurinha Dorival", imagem: dorival, tipo: "professor" },
@@ -16,5 +27,15 @@ export const recompensas = {
   5: { id: 5, nome: "Figurinha Israel", imagem: israel, tipo: "professor" },
   6: { id: 6, nome: "Figurinha Mariany Técnica", imagem: marianyTecnica, tipo: "professor" },
   7: { id: 7, nome: "Badge Chave Primária", imagem: chavePrimaria, tipo: "conceito" },
-  8: { id: 8, nome: "Badge Clean Code", imagem: cleanCode, tipo: "conceito" }
+  8: { id: 8, nome: "Badge Clean Code", imagem: cleanCode, tipo: "conceito" },
+  9: { id: 9, nome: "Figurinha React", imagem: react, tipo: "tecnologia" },
+  10: { id: 10, nome: "Figurinha python", imagem: python, tipo: "tecnologia" },
+  11: { id: 11, nome: "Figurinha Dart", imagem: dart, tipo: "tecnologia" },
+  12: { id: 12, nome: "Figurinha flutter", imagem: flutter, tipo: "tecnologia" },
+  13: { id: 13, nome: "Figurinha Linux", imagem: linux, tipo: "tecnologia" },
+  14: { id: 14, nome: "Figurinha MongoDB", imagem: mongoDB, tipo: "tecnologia" },
+  15: { id: 15, nome: "Figurinha Google,", imagem: google, tipo: "tecnologia" },
+  16: { id: 16, nome: "Figurinha javaScript", imagem: javaScript, tipo: "tecnologia" },
+  17: { id: 17, nome: "Figurinha NPM", imagem: npm, tipo: "tecnologia" },
+  18: { id: 18, nome: "Figurinha Microsoft", imagem: microsoft, tipo: "tecnologia" },
 };
